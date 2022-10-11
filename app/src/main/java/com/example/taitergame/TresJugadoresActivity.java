12485e27b8e7c816cbd3bcef4b0c210e70ca8d9b
RicardoCastillo991 <r.castillo39@alumnos.santotomas.cl>
2022-10-11 13:39:29 -0300

    la parte de ingresar nombre falta en backend pero lo demas va bien

--- app/src/main/java/com/example/taitergame/TresJugadoresActivity.java
+++ app/src/main/java/com/example/taitergame/TresJugadoresActivity.java
@@ -3,13 +3,12 @@ package com.example.taitergame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class CuatroJugadoresActivity extends AppCompatActivity {
-    private Button bt
+public class TresJugadoresActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cuatro_jugadores);
+        setContentView(R.layout.activity_tres_jugadores);
     }
 }
