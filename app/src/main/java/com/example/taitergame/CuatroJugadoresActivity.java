9f5f93144bd9c3bf3b3dbfda534a0a0f89b5d05e
RicardoCastillo991 <r.castillo39@alumnos.santotomas.cl>
2022-10-11 14:25:01 -0300

    se agrego la utilizacion de seekbar con el uso de solo un activity para dos utilidades distintas ademas se agrego el boton para comenzar
    se quito el activity 3 jugadores

--- app/src/main/java/com/example/taitergame/CuatroJugadoresActivity.java
+++ app/src/main/java/com/example/taitergame/CuatroJugadoresActivity.java
@@ -1,15 +1,72 @@
 package com.example.taitergame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class CuatroJugadoresActivity extends AppCompatActivity {
-    private Button bt
+    private Button btnComenzar;
+    private Bundle bundle;
+    private EditText txtJugadorCuatro;
+    private ConstraintLayout colorJugadorCuatro;
+    private TextView txtTiempo;
+    private SeekBar seekBarTiempo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuatro_jugadores);
+        btnComenzar = (Button) findViewById(R.id.btnComenzarPartida);
+
+        //Estos objetos son para el uso de solo un activity para dos condiciones.
+        txtJugadorCuatro = (EditText)findViewById(R.id.txtJugadorCuatro);
+        colorJugadorCuatro = (ConstraintLayout)findViewById(R.id.colorJugador4);
+        //El seekbar y tv son para la medicion y utilizacion del tiempo para cada jugador.
+        txtTiempo = (TextView)findViewById(R.id.txtTiempo);
+        seekBarTiempo = (SeekBar)findViewById(R.id.seekBarTiempo2);
+        //el bundle toma los datos de los botones de formulario para saber cuantos jugadores son.
+        bundle = getIntent().getExtras();
+        String jugadores = bundle.getString("Jugadores");
+
+        int numeroJugadores = Integer.parseInt(jugadores);
+
+        if(numeroJugadores == 3){
+            txtJugadorCuatro.setVisibility(View.INVISIBLE);
+            colorJugadorCuatro.setBackgroundColor(Color.parseColor("#ffffff"));
+        }
+
+        //Adaptacion del limites del tiempo para la seekbar(el juego no puede durar menos de 30 segundos).
+        seekBarTiempo.setMax(330);
+
+        seekBarTiempo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int valor = i + 30;
+                txtTiempo.setText("Segundos: "+(String.valueOf(valor)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/taitergame/FormularioActivity.java
+++ app/src/main/java/com/example/taitergame/FormularioActivity.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class FormularioActivity extends AppCompatActivity {
-    private Button btnDosJugadores, btnCuatroJugadores;
+    private Button btnDosJugadores,btnTresJugadores, btnCuatroJugadores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
         btnDosJugadores = (Button) findViewById(R.id.btnDosJugadores);
+        btnTresJugadores = (Button)findViewById(R.id.btnTresJugadores);
         btnCuatroJugadores = (Button) findViewById(R.id.btnCuatroJugadores);
 
         btnDosJugadores.setOnClickListener(new View.OnClickListener() {
@@ -24,13 +25,25 @@ public class FormularioActivity extends AppCompatActivity {
             }
         });
 
+        btnTresJugadores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FormularioActivity.this, CuatroJugadoresActivity.class);
+                intent.putExtra("Jugadores", "3");
+                startActivity(intent);
+            }
+        });
+
         btnCuatroJugadores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(FormularioActivity.this, CuatroJugadoresActivity.class);
+                intent.putExtra("Jugadores", "4");
                 startActivity(intent);
             }
         });
+
+
 
     }
 }
